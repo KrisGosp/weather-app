c8751a78faba1538e5c609cb0e880bf860509b2e
Kris Gospodinov <kris060601@gmail.com>
2024-02-17 18:33:32 +0200

    Revert "Added React Query"
    This reverts commit 8f6184d8b2de334dd5ce4bed29e631624c037ea6.

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -1,24 +1,21 @@
+import { useEffect, useState } from "react";
 import APIClient from "../services/api-client";
 import { CityQuery } from "../App";
-import { useQuery } from "@tanstack/react-query";
 
 const useLocation = (city: CityQuery) => {
-  // const [location, setLocation] = useState<CityQuery>({
-  //   key: "51097",
-  //   name: "Sofia",
-  // });
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState("");
+  const [location, setLocation] = useState<CityQuery>({
+    key: "51097",
+    name: "Sofia",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const apiClient = new APIClient(
     `/locations/v1/cities/autocomplete?apikey="${
       import.meta.env.VITE_API_KEY
     }&q=${city.name}`
   );
-  return useQuery({
-    queryKey: ["location", city.name],
-    queryFn: apiClient.getLocation,
-  });
+  useEffect(() => )
   //     .get(
   //       "/locations/v1/cities/autocomplete?apikey=" +
   //         import.meta.env.VITE_API_KEY +
@@ -36,7 +33,7 @@ const useLocation = (city: CityQuery) => {
   //     .finally(() => setIsLoading(false));
   // }, [city.name]);
 
-  // return { location, error, isLoading };
+  return { location, error, isLoading };
 };
 
 export default useLocation;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 import "./index.css";
 import theme from "./theme.ts";
 
-const client = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ChakraProvider theme={theme}>
-    <QueryClientProvider client={client}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <App />
   </ChakraProvider>
 );
